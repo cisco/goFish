79812da9a56249f20a93a6f9c2e5cbe570fe6412
Tomas Rigaux <trigaux@cisco.com>
2019-05-08 17:09:22 -0600

    Added more javascript control for the videos and cleaned up some code and files.

--- Server.go
+++ Server.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"io/ioutil"
+	"io"
 	"net/http"
+	"os"
 	"strconv"
-	"strings"
 )
 
 func main() {
@@ -22,63 +22,54 @@ func CreateServer(port uint16) {
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	fs = http.FileServer(http.Dir("static/scripts"))
 	http.Handle("/scripts/", http.StripPrefix("/scripts/", fs))
-	fs = http.FileServer(http.Dir("static/videos"))
+	fs = http.FileServer(http.Dir("static/ videos/"))
 	http.Handle("/videos/", http.StripPrefix("/videos/", fs))
 
 	// Create server in port.
 	http.ListenAndServe(":"+strconv.Itoa(int(port)), nil)
 }
 
-// Function to print to server when no page available.
-func ServerPrint(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Server: This is a Go HTTP Server")
-	fmt.Println("HTTP Request")
-}
-
 // Handles video files uploaded by users using forms on the server.
-func UploadFile(w http.ResponseWriter, r *http.Request, formValue string) string {
+func UploadFile(w http.ResponseWriter, r *http.Request, formValue string) {
 	fmt.Println("File Uploaded.")
 	var format = "mp4"
 
-	// Parse the form, looking for files with max size of 10mb.
-	r.ParseMultipartForm(10 << 20)
+	formData := r.MultipartForm
 
-	file, handler, err := r.FormFile(formValue)
+	files := formData.File["upload-videos"]
 
-	if err != nil {
-		// Print to commnd line.
-		fmt.Println("Error while retrieving the file!")
-		fmt.Println(err)
-		return ""
-	}
-	defer file.Close()
+	for i, _ := range files {
+		file, err := files[i].Open()
+		defer file.Close()
 
-	// Check to make sure that the uploaded file is the correct format.
-	if strings.Contains(strings.ToLower(handler.Filename), format) {
-		fmt.Printf("Uploaded File: %+v\n", handler.Filename)
-		fmt.Printf("Size: %+v\n", handler.Size)
-		fmt.Printf("MIME Header: %+v\n", handler.Header)
-
-		// Create a temporary file with a specific naming convention.
-		tempFile, err := ioutil.TempFile("videos", "video-*."+format)
 		if err != nil {
 			fmt.Println(err)
+			return
 		}
-		defer tempFile.Close()
 
-		fileBytes, err := ioutil.ReadAll(file)
+		out, err := os.Create("static/videos" + "video-*." + format)
+
+		defer out.Close()
+
+		if err != nil {
+			fmt.Println("Unable to create the file to be written. Please enure you have correct write access priviliges.")
+			return
+		}
+
+		_, err = io.Copy(out, file)
+
 		if err != nil {
 			fmt.Println(err)
+			return
 		}
 
-		tempFile.Write(fileBytes)
-		fmt.Println("File uploaded successfully!")
-		return tempFile.Name()
+		fmt.Println("Files uploaded successfully.")
+		fmt.Println("Filename: " + files[i].Filename)
+
 	}
-	fmt.Println("Incorrect file format!")
-	return ""
 }
 
+// If the file is valid, returns the name of the file.
 func GetFileName(r *http.Request, formField string) string {
 	r.ParseMultipartForm(10 << 20)
 	var file, handler, err = r.FormFile(formField)
@@ -108,8 +99,7 @@ func HandleHTMLTemplate(file string) {
 			fmt.Println("Selecting " + leftVideoName)
 			fmt.Println("Selecting " + rightVideoName)
 		} else {
-			leftVideoName = UploadFile(w, r, "upload-video-1")
-			rightVideoName = UploadFile(w, r, "upload-video-2")
+			UploadFile(w, r, "upload-videos")
 		}
 
 		w.Header().Add("Content-Type", "text/html")
--- static/scripts/ProcessVideo.js
+++ static/scripts/ProcessVideo.js
@@ -14,7 +14,7 @@ let processor = {
         }, false);
 
         this.leftVideo.addEventListener('pause', function(){
-            self.rightVideo.pause();
+            if(!self.ended) self.rightVideo.pause();
         }, false);
         this.rightVideo.addEventListener('pause', function(){
             self.timerCallback();
@@ -40,5 +40,29 @@ let processor = {
         // TODO: Replace these with a function that will draw the exact pixels with minimum loss.
         context.drawImage(this.leftVideo, 0, 0, this.canvas.width/2, this.canvas.height);
         context.drawImage(this.rightVideo, this.canvas.width/2, 0, this.canvas.width/2, this.canvas.height);
+
+        // Calculate the length of the combined video.
+        var lOffset = parseFloat(document.getElementById("left-offset").innerText);
+        var rOffset = parseFloat(document.getElementById("right-offset").innerText);
+        var time = ((this.leftVideo.duration - lOffset) + (this.rightVideo.duration - rOffset)) /2
+
+        var scrubber = document.getElementById("curr-time");
+        scrubber.max = time.toString();
+        var max_curr = Math.max(this.leftVideo.currentTime, this.rightVideo.currentTime);
+        var min_curr = Math.min(this.leftVideo.currentTime, this.rightVideo.currentTime)
+        var max_offset = Math.max(lOffset, rOffset), min_offset = Math.min(lOffset, rOffset);
+        scrubber.value = ((max_curr - max_offset) + (min_curr - min_offset)) / 2;
+    },
+
+    pause : function()
+    {
+        this.leftVideo.pause();
+        this.rightVideo.pause();
+    },
+
+    play: function()
+    {
+        this.leftVideo.play();
+        this.rightVideo.play();
     }
 }
